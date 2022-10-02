6a7b96297e8eac320055ec2294cfb210f6761648
admin <puziikmax@gmail.com>
2022-10-02 13:56:46 +0300

    1

--- src/main/java/NotificationToTeamLead.java
+++ src/main/java/NotificationToTeamLead.java
@@ -1,15 +1,11 @@
 import org.quartz.*;
 
-import javax.xml.soap.SOAPConnection;
-import javax.xml.soap.SOAPMessage;
-
 public class NotificationToTeamLead implements Job {
 
 
     @Override
     public void execute(JobExecutionContext jobExecutionContext) {
         System.out.println("TeamLead Trigger");
-
 
     }
 
